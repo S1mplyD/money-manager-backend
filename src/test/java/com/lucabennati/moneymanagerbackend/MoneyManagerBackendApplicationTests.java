88cafaa47fe5adcf4793120a1285b47c55e4475e
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-11-15 10:56:09 +0100

    feat: add default test

---
+++ src/test/java/com/lucabennati/moneymanagerbackend/MoneyManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucabennati.moneymanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
