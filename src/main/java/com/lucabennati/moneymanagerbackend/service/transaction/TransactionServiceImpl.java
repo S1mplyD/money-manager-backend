35a3b4e9b2bac241dfea421138ef0284db81c0e6
Luca Bennati <52350906+S1mplyD@users.noreply.github.com>
2023-11-15 11:03:42 +0100

    feat: add transaction service and its implementation

--- src/main/java/com/lucabennati/moneymanagerbackend/service/transaction/TransactionServiceImpl.java
+++ src/main/java/com/lucabennati/moneymanagerbackend/service/transaction/TransactionServiceImpl.java
@@ -5,7 +5,6 @@ import com.lucabennati.moneymanagerbackend.repository.TransactionRepository;
 import org.bson.codecs.ObjectIdGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
